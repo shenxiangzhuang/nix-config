5b1ebd213a6f80eb22714f9a195524e692b74150
Xiangzhuang Shen <datahonor@gmail.com>
2025-02-05 03:25:52 +0000

    init

---
+++ configuration.nix
@@ -0,0 +1,221 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  # Add to your configuration
+  
+  # 启用 Flakes 特性以及配套的船新 nix 命令行工具
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  environment.systemPackages = with pkgs; [
+    # Flakes 通过 git 命令拉取其依赖项，所以必须先安装好 git
+    git
+    vim
+    wget
+    
+    # 代理设置：mihomo
+    #pkgs.mihomo
+    pkgs.mihomo-party
+    #pkgs.gnomeExtensions.mihomo-tray
+    
+    # 这里从 helix 这个 inputs 数据源安装了 helix 程序
+    #inputs.helix.packages."${pkgs.system}".helix
+
+    # 电源配置用到
+    pkgs.dconf
+  ];
+  # 将默认编辑器设置为 vim
+  environment.variables.EDITOR = "vim";
+
+  # 字体设置  
+  fonts.packages = with pkgs; [
+  fira-code
+  # 其他你喜欢的字体
+  ];
+
+  # 新增用户
+  users.users.mathew = {
+    isNormalUser = true;
+    home = "/home/mathew";
+    description = "Mathew in NixOS";
+    extraGroups = [ "users" "wheel" ];  # Add "wheel" for sudo access if needed
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM9cmFXetj0Cgqb0bfHhDrc2aZv64tKouJUvppBcOdck datahonor@gmail.com" ];
+  };
+  
+  # 启用 OpenSSH 后台服务
+  services.openssh = {
+    enable = true;
+    settings = {
+      X11Forwarding = true;
+      PermitRootLogin = "no"; # disable root login
+      PasswordAuthentication = false; # disable password login
+    };
+    openFirewall = true;
+  };
+  nix = {
+    settings = {
+      substituters = ["https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"];
+    };
+  };
+  
+  # -------------------------------------------
+  # 电源管理设置（禁用休眠和睡眠）
+  # -------------------------------------------
+  # 关闭系统休眠（Hibernate）功能
+  boot.kernelParams = [ "nohibernate" ];
+
+  # 禁用挂起（Suspend）和休眠（Hibernate）
+  services.logind = {
+    lidSwitch = "ignore";              # 合盖不执行任何操作（笔记本）
+    lidSwitchExternalPower = "ignore"; # 外接电源时合盖不操作
+    extraConfig = ''
+      HandlePowerKey=ignore   # 按电源键不操作
+      HandleSuspendKey=ignore # 禁用挂起按键
+      HandleHibernateKey=ignore
+      IdleAction=ignore       # 空闲时不执行任何动作
+      IdleActionSec=0
+    '';
+  };
+
+  # -------------------------------------------
+  # X11 桌面环境设置（通用熄屏配置）
+  # -------------------------------------------
+  services.xserver = {
+    enable = true;
+    
+    # 禁用屏幕保护及自动熄屏（适用于非桌面环境管理的情况）
+    serverFlagsSection = ''
+      Option "BlankTime" "0"    # 关闭屏幕时间（0=永不）
+      Option "StandbyTime" "0"  # 待机时间
+      Option "SuspendTime" "0"  # 挂起时间
+      Option "OffTime" "0"      # 关闭屏幕
+    '';
+  };
+
+  # -------------------------------------------
+  # 桌面环境特定配置（根据实际使用选择）
+  # -------------------------------------------
+  # 配置 GNOME 的电源管理
+  services.xserver.displayManager.gdm.autoSuspend = false;
+
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # hardware.pulseaudio.enable = true;
+  # OR
+  # services.pipewire = {
+  #   enable = true;
+  #   pulse.enable = true;
+  # };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.users.alice = {
+  #   isNormalUser = true;
+  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  #   packages = with pkgs; [
+  #     tree
+  #   ];
+  # };
+
+  # programs.firefox.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #   wget
+  # ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
+
